f47f770d078d556da46cb6b51f8eb2c8218e7208
Junayd H <junaydhus@gmail.com>
2023-06-20 15:04:07 -0700

    add basic logic and explanation for counting frequency

--- arrays-hashing/topKFrequentElements.js
+++ arrays-hashing/topKFrequentElements.js
@@ -4,5 +4,15 @@
  * @return {number[]}
  */
  var topKFrequent = function(nums, k) {
+    // create result array;
+    let result = [];
+    // create dictionary to hold integers and their frequency as key:value pairs
+    let numCount = {};
+    // loop through nums array
+    for (const num of nums) {
+      // check if num exists as key in numCount, if yes increment, if no create it with value of 1
+      numCount[num] ? numCount[num]++ : numCount[num] = 1;
+    }
     
+
 };
