870778be83c9f8ee111994654a6e9c7f22f2f20d
Junayd H <junaydhus@gmail.com>
2023-06-21 12:57:12 -0700

    add helper array to group numbers with the same counts

--- arrays-hashing/topKFrequentElements.js
+++ arrays-hashing/topKFrequentElements.js
@@ -4,8 +4,14 @@
  * @return {number[]}
  */
  var topKFrequent = function(nums, k) {
-    // create result array;
+    // create result array
     let result = [];
+    // create helper array
+    let helper = [];
+    // loop and fill with unique empty arrays
+    for (let i = 0; i < nums.length + 1; i++) {
+      helper[i] = [];
+    }
     // create dictionary to hold integers and their frequency as key:value pairs
     let numCount = {};
     // loop through nums array
@@ -13,6 +19,9 @@
       // check if num exists as key in numCount, if yes increment, if no create it with value of 1
       numCount[num] ? numCount[num]++ : numCount[num] = 1;
     }
-    
-
+    // loop through dictionary items
+    for (const [key, val] of Object.entries(numCount)) {
+      // push num to value(array) of helper array at index [count]
+      helper[val].push(key);
+    }
 };
