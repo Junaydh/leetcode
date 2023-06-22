b8f55b38e46b056c9fa003c3669d422ea3fec470
Junayd H <junaydhus@gmail.com>
2023-06-21 23:30:07 -0700

    remove nested for-loop to sorted dictionary for better time-complexity

--- arrays-hashing/topKFrequentElements.js
+++ arrays-hashing/topKFrequentElements.js
@@ -6,34 +6,30 @@
  var topKFrequent = function(nums, k) {
     // create result array
     let result = [];
+
     // create helper array
     let helper = [];
-    // loop and fill with unique empty arrays
-    for (let i = 0; i < nums.length + 1; i++) {
-      helper[i] = [];
-    }
+
     // create dictionary to hold integers and their frequency as key:value pairs
     let numCount = {};
+
     // loop through nums array
     for (const num of nums) {
-      // check if num exists as key in numCount, if yes increment, if no create it with value of 1
+      // check if num exists as key in numCount, if true increment, if false create it with value of 1
       numCount[num] ? numCount[num]++ : numCount[num] = 1;
     }
-    // loop through dictionary items
-    for (const [key, val] of Object.entries(numCount)) {
-      // push num to value(array) of helper array at index [count]
-      helper[val].push(key);
+
+    // populate helper array with numcount entries
+    helper = [...numCount.entries()];
+
+    // sort helper array by frequency in descending order
+    helper.sort((a, b) => b[1] - a[1]);
+
+    // loop through helper array
+    for (let i = 0; i < k; i++) {
+      // push integer value into results array
+      result.push(helper[i][0]);
     }
-    // loop backwards through helper array (largest count to smallest count)
-    for (let i = helper.length -1; i > 0; i--) {
-      // loop through each value in nested array
-      for (let num of helper[i]) {
-        // push that value to results array
-        result.push(num);
-        // if length of result array = k, return
-        if (result.length === k) {
-          return result;
-        }
-      }
-    }
+    // return result array
+    return result;
 };
