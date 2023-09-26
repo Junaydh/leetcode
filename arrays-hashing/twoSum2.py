400d1231d468aaece008d311edb93b6d490c05a6
Junayd H <junaydhus@gmail.com>
2023-09-26 12:36:21 -0700

    added var to compute current sum once per loop instead of 3

--- arrays-hashing/twoSum2.py
+++ arrays-hashing/twoSum2.py
@@ -3,9 +3,10 @@ class Solution:
       l, r = 0, len(numbers) - 1
 
       while l < r:
-        if numbers[l] + numbers[r] == target:
+        cSum = numbers[l] + numbers[l]
+        if cSum == target:
           return [l+1, r+1]
-        elif numbers[l] + numbers[r] < target:
+        elif cSum < target:
           l += 1
-        elif numbers[l] + numbers[r] > target:
+        elif cSum > target:
           r -= 1
