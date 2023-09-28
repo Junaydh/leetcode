1d21e414330d18db7aceddb5580431f6e126715d
Junayd H <junaydhus@gmail.com>
2023-09-28 13:33:02 -0700

    pseudo code solution

--- 2pointer/trappingRainWater.py
+++ 2pointer/trappingRainWater.py
@@ -1,2 +1,14 @@
 class Solution:
     def trap(self, height: List[int]) -> int:
+      pass
+          
+
+
+
+
+    # 2 pointer solution
+    # l = 0, r = l+1
+    # while height[r] < height[l] trappedRain += height[l] - height[r]
+    # shift right pointer until height[r] > height[l]
+    # l = r, r = l+1
+    # return trapped rainwater
