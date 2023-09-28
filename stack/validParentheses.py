22a9c16b75756a6ee01bfaca36b025fe2b94d776
Junayd H <junaydhus@gmail.com>
2023-09-28 15:39:57 -0700

    solution

--- stack/validParentheses.py
+++ stack/validParentheses.py
@@ -1,11 +1,19 @@
 class Solution:
     def isValid(self, s: str) -> bool:
-      pass
+      stack = []
+      hash = {")": "(", "}" : "{", "]": "["}
 
-
+      for char in s:
+        if char not in hash:
+          stack.append(char)
+          continue
+        if not stack or stack[-1] != hash[char]:
+            return False
+        stack.pop()
+      
+      return not stack
     # create hash with closing brackets as keys and corresponding opening brackets as values
     # loop through the string
     # while current char is not in our hash, add character to the stack
     # if it is in our hash, check that the last char added to our stack is it's corresponding opening bracket and pop
     # continue until stack is empty
-    
