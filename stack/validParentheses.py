408d61b0f39baf7313ca7bac5e9df4f4154c3986
Junayd H <junaydhus@gmail.com>
2023-09-28 15:25:34 -0700

    add pseudo code solution

--- stack/validParentheses.py
+++ stack/validParentheses.py
@@ -1,3 +1,11 @@
 class Solution:
     def isValid(self, s: str) -> bool:
-        
+      pass
+
+
+    # create hash with closing brackets as keys and corresponding opening brackets as values
+    # loop through the string
+    # while current char is not in our hash, add character to the stack
+    # if it is in our hash, check that the last char added to our stack is it's corresponding opening bracket and pop
+    # continue until stack is empty
+    
