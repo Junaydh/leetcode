e8f729f09d6f1633a86957310c96f31558ac662e
Junayd H <junaydhus@gmail.com>
2023-09-28 15:41:30 -0700

    added pseudo code

--- stack/validParentheses.py
+++ stack/validParentheses.py
@@ -17,3 +17,4 @@ class Solution:
     # while current char is not in our hash, add character to the stack
     # if it is in our hash, check that the last char added to our stack is it's corresponding opening bracket and pop
     # continue until stack is empty
+    # return not stack because if stack is still populated after finishign the loop, our parentheses are not valid
